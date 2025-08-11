ed2252211b9d8eaa2bb14d42e57d7ac3de52c952
jonas <jonas.voisard@gmail.com>
2025-08-11 11:53:49 +0200

    basic members pages

--- src/routes/board/+page.svelte
+++ src/routes/board/+page.svelte
@@ -1,18 +1,86 @@
 <script lang="ts">
   import Member from "./Member.svelte";
+
+  const members: {
+    name: string;
+    title: string;
+    image: string;
+    description: string;
+  }[] = [
+    {
+      name: "Natou",
+      title: "Président de la cours",
+      image: "/natou.jpg",
+      description:
+        "Chef d’orchestre de cette joyeuse équipe, il veille à ce que tout fonctionne dans la bonne humeur et avec efficacité.",
+    },
+    {
+      name: "Lucien",
+      title: "Président de jeunesse + infrastructure",
+      image: "/lucien.jpg",
+      description:
+        "Pilier du groupe, il jongle entre la gestion du groupe jeunesse et la construction des infrastructures.",
+    },
+    {
+      name: "Romain",
+      title: "Secrétaire",
+      image: "/romain.jpg",
+      description:
+        "A l’organisation, il gère tout ce qui est administratif. Il s’occupe également de la comptabilité et du financement.",
+    },
+    {
+      name: "Célien",
+      title: "Technicien",
+      image: "/celien.jpg",
+      description:
+        "La tête dans les fils électriques, il est responsable de tout ce qui touche au web, aux systèmes informatiques et au chronométrage.",
+    },
+    {
+      name: "Pablo",
+      title: "Communication",
+      image: "/pablo.jpg",
+      description:
+        "Véritable porte-parole du groupe, il place les mots et les images pour mettre en avant l’équipe sur tous les supports.",
+    },
+    {
+      name: "Noa",
+      title: "Financement",
+      image: "/noa.jpg",
+      description:
+        "Trouver des financements, négocier des partenariats et dénicher les meilleures opportunités, c’est son domaine.",
+    },
+    {
+      name: "Jimmy",
+      title: "Chalengeur",
+      image: "/jimmy.jpg",
+      description:
+        "Toujours prêt à repousser les limites et à relever de nouveaux défis, casse-cou de la bande, il est celui qui respire l’adrénaline de la course.",
+    },
+    {
+      name: "Maxime",
+      title: "Constructeur",
+      image: "/maxime.jpg",
+      description:
+        "Ingénieux et créatif, il imagine, conçoit et construit tout ce dont l’équipe a besoin.  Les véhicules, les rampes, tout ou presque passe sous son poste à souder.",
+    },
+    {
+      name: "Johan",
+      title: "Mécano",
+      image: "/johan.jpg",
+      description:
+        "Passionné, il connaît chaque boulon et sur le bout des doigts. Réparer, optimiser, crée, il est toujours prêt à mettre les mains dans le cambouis.",
+    },
+  ];
 </script>
 
-<div class="max-w-2xl m-auto p-4 py-32 space-y-16">
+<div class="max-w-2xl m-auto p-4 py-32 space-y-12">
   <section>
     <h2 class="text-4xl font-bold mb-4">L'équipe</h2>
-    <enhanced:img src="$lib/assets/board.jpg" alt="Photo du comité" />
   </section>
 
-  <Member name="Jonas" src="https://api.dicebear.com/9.x/big-smile/svg" />
-  <Member
-    name="Jonas"
-    src="https://api.dicebear.com/9.x/big-smile/svg"
-    reverse
-  />
-  <Member name="Jonas" src="https://api.dicebear.com/9.x/big-smile/svg" />
+  {#each members as member, index}
+    <Member {...member} reverse={!(index % 2)} />
+  {/each}
+
+  <img src="/board.jpg" alt="Comité de la Tchaiscente" />
 </div>
--- src/routes/board/Member.svelte
+++ src/routes/board/Member.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
+  import type { Snippet } from "svelte";
+
   let {
     name,
-    src,
+    title,
+    description,
+    image,
     reverse = false,
-  }: { name: string; src: string; reverse?: boolean } = $props();
+  }: {
+    name: string;
+    title: string;
+    description: string;
+    image: string;
+    reverse?: boolean;
+  } = $props();
 </script>
 
-<section class="flex gap-6 p-4 items-center" class:flex-row-reverse={reverse}>
-  <img {src} alt="Portrait de {name}" class="w-1/3" />
+<section class="flex gap-6 items-center" class:flex-row-reverse={reverse}>
+  <img src={image} alt="Portrait de {name}" class="w-1/3" />
   <div>
-    <p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore dolorum
-      natus alias rem perspiciatis praesentium eaque sit repudiandae nam!
-      Quisquam odio exercitationem ullam animi vel, autem laudantium aliquam
-      eius saepe.
-    </p>
+    <h3 class="text-xl">{title}</h3>
+    <p class="py-3">{description}</p>
   </div>
 </section>
