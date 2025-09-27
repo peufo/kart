7ccc58bf9043400386c95462a3383fd4e239a099
jonas <jonas.voisard@gmail.com>
2025-09-27 10:32:30 +0200

    better error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
+<div class="grid place-content-center p-4" style="height: calc(100vh - 352px);">
+  <div class="border shadow px-12 py-8 text-center rounded-xl bg-base-200">
+    <h2 class="text-2xl">Error {$page.status}</h2>
+    <p>{$page.error?.message}</p>
+    <a href="/" class="btn btn-outline mt-6">Retour à l'accueil</a>
+  </div>
+</div>
